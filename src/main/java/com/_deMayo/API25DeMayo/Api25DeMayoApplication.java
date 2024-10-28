81b6c6ed641eda8ca381bc4140c1bec6e65562ee
JuanIgnacio2 <juanignacioleivachaves@gmail.com>
2024-10-28 13:52:13 -0300

    Proyecto añadido

---
+++ src/main/java/com/_deMayo/API25DeMayo/Api25DeMayoApplication.java
@@ -0,0 +1,13 @@
+package com._deMayo.API25DeMayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api25DeMayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api25DeMayoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_deMayo/API25DeMayo/Api25DeMayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._deMayo.API25DeMayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api25DeMayoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
